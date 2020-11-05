ea9a4bfe615e9e6255208746911c8cc89dc3c16d
dev <smirnovskoe@gmail.com>
2020-11-05 10:48:43 -0800

    added inner_db list

--- app.py
+++ app.py
@@ -2,6 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
+inner_db = [
+    {
+        'id': 1,
+        'singer': 'Sting',
+        'song': 'Desert Rose'
+    },
+    {
+        'id': 2,
+        'singer': 'Boney M',
+        'song': 'Rasputin'
+    },
+    {
+        'id': 3,
+        'singer': 'Bob Dylan',
+        'song': 'Knockin\'On Heaven\'s Door'
+    },
+    {
+        'id': 4,
+        'singer': 'Guns & Roses',
+        'song': 'November Rain'
+    },
+]
 
 @app.route('/')
 def index():
